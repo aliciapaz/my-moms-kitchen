290583d0fe77d9f688df204c86a5b9acfd254ae5
aliciapaz <aliciapazrojas@gmail.com>
2021-05-03 16:11:08 -0400

    update webpack config to load assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
- module.exports = {
-  mode: 'development',
+module.exports = {
+  mode: "development",
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
+    home: './src/home.js',
   },
-    plugins: [
+  output: {
+    filename: "[name].main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: "Output Management",
     }),
   ],
-   output: {
-     filename: '[name].main.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+};
