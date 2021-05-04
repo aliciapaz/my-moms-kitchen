ed1616f9fc66f6380ef8d0f826644c244be99cfe
aliciapaz <aliciapazrojas@gmail.com>
2021-05-04 18:14:12 -0400

    add animation to dropdown menu

--- src/tabs.js
+++ src/tabs.js
@@ -1,35 +1,37 @@
 export function navTab() {
   const element = document.createElement("nav");
+  
+  const toggleBtn = document.createElement("div"); 
   const tabs = document.createElement("ul");
-  const toggleBtn = document.createElement("div");
-  const burgerIcon = document.createElement("i");
-  burgerIcon.className = "fas fa-bars burger-btn";
   tabs.className = "nav-links";
-
   const menuTab = document.createElement("li");
   const aboutTab = document.createElement("li");
   const contactTab = document.createElement("li");
   const homeTab = document.createElement("li");
+  
+  const burgerIcon = document.createElement("i");
+  burgerIcon.className = "fas fa-bars burger-btn";
+  toggleBtn.appendChild(burgerIcon);
+  
+  toggleBtn.onclick = () => {
+    if (tabs.className === "nav-links") {
+      tabs.className = "responsive";
+    } else {
+      tabs.className = "nav-links"
+    }
+  };
 
   homeTab.innerHTML = "Home";
   menuTab.innerHTML = "Menu";
   aboutTab.innerHTML = "About";
   contactTab.innerHTML = "Contact";
 
-  toggleBtn.appendChild(burgerIcon);
-  toggleBtn.onclick = () => {
-    homeTab.style.display = 'block';
-    menuTab.style.display = 'block';
-    aboutTab.style.display = 'block';
-    contactTab.style.display = 'block';
-  };
-
   tabs.appendChild(homeTab);
   tabs.appendChild(menuTab);
   tabs.appendChild(aboutTab);
   tabs.appendChild(contactTab);
-  tabs.appendChild(toggleBtn);
 
+  element.appendChild(toggleBtn);
   element.appendChild(tabs);
   return element;
 }
