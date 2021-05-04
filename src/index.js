bcb0dced6b65c9fff07822d390506761e7edec2c
aliciapaz <aliciapazrojas@gmail.com>
2021-05-04 12:36:47 -0400

    add fontawesome icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import 'reseter.css'
 import './style.css';
-import { component } from './home.js';
 
+import { component } from './home.js';
+import { navTab } from './tabs.js';
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+document.body.appendChild(navTab());
 document.body.appendChild(component());
---
+++ src/tabs.js
@@ -0,0 +1,26 @@
+export function navTab() {
+  const element = document.createElement("nav");
+  const tabs = document.createElement("ul");
+  const burgerBtn = document.createElement('i');
+  burgerBtn.className = 'fas fa-bars';
+  tabs.className = 'nav-links'
+
+  const menuTab = document.createElement("li");
+  const aboutTab = document.createElement("li");
+  const contactTab = document.createElement("li");
+  const homeTab = document.createElement("li");
+
+  homeTab.innerHTML = 'Home';
+  menuTab.innerHTML = 'Menu';
+  aboutTab.innerHTML = 'About';
+  contactTab.innerHTML = 'Contact';
+
+  tabs.appendChild(homeTab);
+  tabs.appendChild(menuTab);
+  tabs.appendChild(aboutTab);
+  tabs.appendChild(contactTab);
+  tabs.appendChild(burgerBtn);
+  
+  element.appendChild(tabs);
+  return element;
+}
