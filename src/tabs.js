379672d90447319189a27837bd0f0694beb563eb
aliciapaz <aliciapazrojas@gmail.com>
2021-05-04 14:12:16 -0400

    make navbar appear on click

--- src/tabs.js
+++ src/tabs.js
@@ -1,32 +1,35 @@
 export function navTab() {
   const element = document.createElement("nav");
   const tabs = document.createElement("ul");
-  const burgerBtn = document.createElement('i');
-  burgerBtn.className = 'fas fa-bars burger-btn';
-  burgerBtn.onclick = displayLinks();
-  tabs.className = 'nav-links'
+  const toggleBtn = document.createElement("div");
+  const burgerIcon = document.createElement("i");
+  burgerIcon.className = "fas fa-bars burger-btn";
+  tabs.className = "nav-links";
 
   const menuTab = document.createElement("li");
   const aboutTab = document.createElement("li");
   const contactTab = document.createElement("li");
   const homeTab = document.createElement("li");
 
-  homeTab.innerHTML = 'Home';
-  menuTab.innerHTML = 'Menu';
-  aboutTab.innerHTML = 'About';
-  contactTab.innerHTML = 'Contact';
+  homeTab.innerHTML = "Home";
+  menuTab.innerHTML = "Menu";
+  aboutTab.innerHTML = "About";
+  contactTab.innerHTML = "Contact";
+
+  toggleBtn.appendChild(burgerIcon);
+  toggleBtn.onclick = () => {
+    homeTab.style.display = 'block';
+    menuTab.style.display = 'block';
+    aboutTab.style.display = 'block';
+    contactTab.style.display = 'block';
+  };
 
   tabs.appendChild(homeTab);
   tabs.appendChild(menuTab);
   tabs.appendChild(aboutTab);
   tabs.appendChild(contactTab);
-  tabs.appendChild(burgerBtn);
-  
+  tabs.appendChild(toggleBtn);
+
   element.appendChild(tabs);
-
-  function displayLinks() {
-
-  }
-
   return element;
 }
