bd721ba6a38e8a7d424d6f6dea41677757644709
aliciapaz <aliciapazrojas@gmail.com>
2021-05-05 01:12:48 -0400

    add heading ribbon

--- src/home.js
+++ src/home.js
@@ -1,19 +1,46 @@
-import Kitchen from './images/cocina.jpg'
+import Kitchen from "./images/cocina.jpg";
+import Board from "./images/chalkboard.jpg";
 
-export function component() {
-  const element = document.createElement('div');
-  const cover = document.createElement('div');
-  const header = document.createElement('h1');
-  element.className = 'home-image';
-  cover.className = 'cover';
-  header.innerHTML = "La cocina de Fabiola";
-  
+export function background() {
+  const element = document.createElement("div");
+  const cover = document.createElement("div");
+
+  element.className = "home-image";
+  cover.className = "cover";
+
   const myKitchen = new Image();
-  myKitchen.src= Kitchen;
+  myKitchen.src = Kitchen;
 
   element.appendChild(myKitchen);
   element.appendChild(cover);
-  element.appendChild(header);
+  const ribbonCont = document.createElement('div')
+  const ribbon = document.createElement('h1');
+  const header = document.createElement("strong");
+  ribbonCont.className = 'ribbon-container'
+  ribbon.className = 'ribbon'
+  header.className = 'ribbon-content'
+  header.innerHTML = "La cocina de Fabiola";
+  ribbon.appendChild(header);
+  ribbonCont.appendChild(ribbon)
+  element.appendChild(ribbonCont)
+  return element;
+}
+
+export function home() {
+  const myBoard = new Image();
+  myBoard.src = Board;
+  const element = document.createElement("div");
+  const container = document.createElement("div");
+  element.className = "home-content";
+  container.className = "board-container";
+  
+  const text = document.createElement("h2");
+  
+  text.innerHTML = "Mis platos favoritos hechos por mi mamá";
+  
+  container.appendChild(text);
+  container.appendChild(myBoard);
+  element.appendChild(container);
 
   return element;
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import 'reseter.css'
 import './style.css';
 
-import { component } from './home.js';
+import { background, home } from './home.js';
 import { navTab } from './tabs.js';
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 document.body.appendChild(navTab());
-document.body.appendChild(component());
+document.body.appendChild(background());
+document.body.appendChild(home());
