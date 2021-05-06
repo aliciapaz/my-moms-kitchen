f524e6997247ec8d38c054a79333914a7f096641
aliciapaz <aliciapazrojas@gmail.com>
2021-05-05 22:02:38 -0400

    add contact page

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,13 @@
+import Map from './images/the-map.jpg'
+
 export function contact() {
   const element = document.createElement("div");
   const content = document.createElement("div");
+  const myMap = new Image();
+  myMap.src = Map;
+  
   content.className = "content";
+  content.appendChild(myMap);
   element.className = "flex-container";
 
   const text = document.createElement("h2");
